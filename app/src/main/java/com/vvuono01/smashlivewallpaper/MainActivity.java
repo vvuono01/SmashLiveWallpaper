e3cbd4cc54baf44c26be09b9150df287f5fb5aa9
Vince Vuono <vvuono01@gmail.com>
2018-08-21 12:55:10 -0400

    Basic project setup

---
+++ app/src/main/java/com/vvuono01/smashlivewallpaper/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vvuono01.smashlivewallpaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
